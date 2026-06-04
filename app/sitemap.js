825a6d9f05d0d1a6b4bf9aed08803ebe0d31aa17
Marcoseliecer8 <marcos.eliecer8@gmail.com>
2026-06-04 10:55:43 -0400

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,32 @@
 
+const BASE = "https://usetooly.io";
+
+const tools = [
+  "cover-letter",
+  "email-rewriter",
+  "essay-intro",
+  "youtube-titles",
+  "cold-email",
+  "complaint-letter",
+  "resignation-letter",
+  "out-of-office",
+  "freelance-rate",
+  "performance-review",
+];
+
+export default function sitemap() {
+  const now = new Date();
+  const home = {
+    url: BASE,
+    lastModified: now,
+    changeFrequency: "weekly",
+    priority: 1,
+  };
+  const toolPages = tools.map((slug) => ({
+    url: `${BASE}/${slug}`,
+    lastModified: now,
+    changeFrequency: "monthly",
+    priority: 0.8,
+  }));
+  return [home, ...toolPages];
+}
